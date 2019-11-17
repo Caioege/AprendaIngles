c3d80fd4e6651319c68fa55aa34bc0d0769ab2cf
Caio Henrick <caiohenrick231@gmail.com>
2019-11-17 11:19:30 -0300

    adicionado tela com a lista de licoes e chamado para as telas. adicionado no manifest também a orientacao da aplicacao para travar no modo portrait

--- app/src/main/java/br/com/aprendainglesfanap/LicoesActivity.java
+++ app/src/main/java/br/com/aprendainglesfanap/LicoesActivity.java
@@ -2,13 +2,38 @@ package br.com.aprendainglesfanap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class LicoesActivity extends AppCompatActivity {
+public class LicoesActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);getSupportActionBar().hide();//FULLSCREEN
         setContentView(R.layout.activity_licoes);
+
+        Button fruta = (Button) findViewById(R.id.btn_fruta);
+        fruta.setOnClickListener(this);
+
+        Button animal = (Button) findViewById(R.id.btn_animal);
+        animal.setOnClickListener(this);
+
+        Button parente = (Button) findViewById(R.id.btn_parente);
+        parente.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btn_fruta:
+                Intent intent = new Intent(this, LicaoFrutasActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
