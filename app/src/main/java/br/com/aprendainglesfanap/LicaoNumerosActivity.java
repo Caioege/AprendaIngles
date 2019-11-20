51cd3751a6688a2c66820df70cdb2c2dd4e48f0a
unknown <juliocezar_1991@hotmail.com>
2019-11-20 19:30:02 -0300

    Tela Questionario Numeros

--- app/src/main/java/br/com/aprendainglesfanap/LicaoNumerosActivity.java
+++ app/src/main/java/br/com/aprendainglesfanap/LicaoNumerosActivity.java
@@ -1,17 +1,39 @@
 package br.com.aprendainglesfanap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class LicaoNumerosActivity extends AppCompatActivity {
+public class LicaoNumerosActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);getSupportActionBar().hide();
         setContentView(R.layout.activity_licao_numeros);
+
+        Button questionario4 = (Button) findViewById(R.id.btn_questionario4);
+        questionario4.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btn_questionario4:
+                Intent intentQuestionario4 = new Intent(this, QuestionarioNumerosActivity.class);
+                startActivity(intentQuestionario4);
+                break;
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
--- app/src/main/java/br/com/aprendainglesfanap/QuestionarioNumerosActivity.java
+++ app/src/main/java/br/com/aprendainglesfanap/QuestionarioNumerosActivity.java
@@ -1,4 +1,17 @@
 package br.com.aprendainglesfanap;
 
-public class QuestionarioNumerosActivity {
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class QuestionarioNumerosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);getSupportActionBar().hide();
+        setContentView(R.layout.activity_questionario_numeros);
+    }
 }
